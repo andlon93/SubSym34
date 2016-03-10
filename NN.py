9d98c99354974ca9604fd03c62bfe2f1002941d8
andlon93 <andreas.lones@gmail.com>
2016-03-10 12:42:05 +0100

    forward_prop works

--- NN.py
+++ NN.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import logistic
 #
 class NN:
 	weights = None
@@ -6,19 +7,28 @@ class NN:
 	def __init__(self, weights):
 		self.weights = np.asarray(weights)
 	#
-	def sigmoid(self):
-		pass
+	def sigmoid(self, x):
+		return logistic.cdf(x)
 	#
-	def soft_max(self):
-		pass
+	def soft_max(self, x):
+		return x
 	#
-	def forward_propagation(innput, a_Func, soft_max):
+	def forward_propagation(self, innput):
 		innput = np.asarray(innput)
-		for weight in weights:
+		for weight in self.weights:
 			innput = np.dot(innput,weight)
 			# Run activation function
-			for i in range(len(temp)):
-				innput[i] = a_Func(innput[i])
+			for i in range(len(innput)):
+				innput[i] = self.sigmoid(innput[i])
 			#
-		output = soft_max(temp)
-				
+		output = self.soft_max(innput)
+		print(output)
+		return np.argmax(output)
+if __name__ == '__main__':
+	import Game as G
+	import FlatLandEA as FL
+	fl = FL.individual(0.1, [6,3])
+	print(fl.genotype)
+	nn = NN(fl.genotype)
+	print( nn.forward_propagation( [1,1,1,1,1,1] ) )
+	
