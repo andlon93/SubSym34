f91d9059885061fe55f0345ee14114ee13dc7682
olehalvor <olli408@hotmail.com>
2016-03-10 08:16:45 +0100

    nearby values actually working

--- Game.py
+++ Game.py
@@ -15,8 +15,8 @@ class game:
 	def generateBoard(self,foodProb,poisonProb,length):
 		self.board = [[0 for x in range(length)] for k in range(length) ]
 		player_placed = False
-		for k in range(length):
-			for i in range(length):
+		for i in range(length):
+			for k in range(length):
 				foodRng = rng.random()
 				poisonRng = rng.random()
 				if rng.random()>foodProb and foodRng>poisonRng:
@@ -26,8 +26,50 @@ class game:
 				if player_placed==False and self.board[i][k]==0:
 					self.board[i][k] = 3
 					player_placed=True
-					self.player_pos=[k,i]
+					self.player_pos=[i,k]
 
+	def move(self,dir):
+		#dir: -1=left, 0=forward, 1=right
+		player_dir = self.player_dir
+		player_pos = self.player_pos
+		if player_dir==-1:
+			if dir==-1:
+				player_dir=2
+			elif dir==1:
+				player_dir=0
+		if player_dir==0:
+			if dir==-1:
+				player_dir=-1
+			elif dir==1:
+				player_dir=1
+		if player_dir==1:
+			if dir==-1:
+				player_dir=0
+			elif dir==1:
+				player_dir=2
+		if player_dir==2:
+			if dir==-1:
+				player_dir=1
+			elif dir==1:
+				player_dir=-1
+		if player_dir==-1:
+			self.board[player_pos[0]][player_pos[1]]=0
+			player_pos[0]=player_pos[0]-1
+			self.board[player_pos[0]][player_pos[1]]=3
+		if player_dir==0:
+			self.board[player_pos[0]][player_pos[1]]=0
+			player_pos[1]=player_pos[1]+1
+			self.board[player_pos[0]][player_pos[1]]=3
+		if player_dir==1:
+			self.board[player_pos[0]][player_pos[1]]=0
+			player_pos[0]=player_pos[0]+1
+			self.board[player_pos[0]][player_pos[1]]=3
+		if player_dir==2:
+			self.board[player_pos[0]][player_pos[1]]=0
+			player_pos[1]=player_pos[1]-1
+			self.board[player_pos[0]][player_pos[1]]=3
+		self.player_pos = player_pos
+		self.player_dir = player_dir
 
 	def getNearbyTiles(self):
 		# Returns in order of facing: left, up, right
@@ -36,33 +78,33 @@ class game:
 		player_pos = self.player_pos
 		nearby = []
 		if player_dir == 0:  #up
-			tile2Y = player_pos[0]-1
-			tile2X = player_pos[1]
-			tile1Y = player_pos[0]
-			tile1X = player_pos[1]-1
-			tile3Y = player_pos[0]+1
-			tile3X = player_pos[1]
+			tile1X = player_pos[0]
+			tile1Y = player_pos[1]-1
+			tile2X = player_pos[0]-1
+			tile2Y = player_pos[1]
+			tile3X = player_pos[0]
+			tile3Y = player_pos[1]+1
 		if player_dir == 1: #Right
-			tile3Y = player_pos[0]
-			tile3X = player_pos[1]+1
-			tile2Y = player_pos[0]
-			tile2X = player_pos[1]-1
-			tile1Y = player_pos[0]+1
-			tile1X = player_pos[1]
+			tile1X = player_pos[0]-1
+			tile1Y = player_pos[1]
+			tile2X = player_pos[0]
+			tile2Y = player_pos[1]+1
+			tile3X = player_pos[0]+1
+			tile3Y = player_pos[1]
 		if player_dir == -1: #Left
-			tile2Y = player_pos[0]
-			tile2X = player_pos[1]+1
-			tile3Y = player_pos[0]
-			tile3X = player_pos[1]-1
-			tile1Y = player_pos[0]-1
-			tile1X = player_pos[1]
+			tile1X = player_pos[0]+1
+			tile1Y = player_pos[1]
+			tile2X = player_pos[0]
+			tile2Y = player_pos[1]-1
+			tile3X = player_pos[0]-1
+			tile3Y = player_pos[1]
 		if player_dir == 2: #Down
-			tile3Y = player_pos[0]-1
-			tile3X = player_pos[1]
-			tile1Y = player_pos[0]
-			tile1X = player_pos[1]+1
-			tile2Y = player_pos[0]+1
-			tile2X = player_pos[1]
+			tile1X = player_pos[0]
+			tile1Y = player_pos[1]+1
+			tile2X = player_pos[0]+1
+			tile2Y = player_pos[1]
+			tile3X = player_pos[0]
+			tile3Y = player_pos[1]-1
 
 		if tile1X>length-1:
 			tile1X=tile1X-length
@@ -77,8 +119,8 @@ class game:
 		if tile3Y>length-1:
 			tile3Y=tile3Y-length
 
-		nearby.append(self.board[tile2X][tile2Y])
 		nearby.append(self.board[tile1X][tile1Y])
+		nearby.append(self.board[tile2X][tile2Y])
 		nearby.append(self.board[tile3X][tile3Y])
 		return nearby
 
@@ -88,7 +130,13 @@ class game:
 if __name__ == '__main__':
 	b = game()
 	b.generateBoard((1/3),(1/3),6)
+	b.player_dir=2
 	for i in range(len(b.board)):
 		print (b.board[i])
-	b.player_dir=2
 	print (b.getNearbyTiles())
+	print (b.player_pos)
+	# b.move(0)
+	# for i in range(len(b.board)):
+	# 	print (b.board[i])
+	# print (b.getNearbyTiles())
+	# print (b.player_pos)
